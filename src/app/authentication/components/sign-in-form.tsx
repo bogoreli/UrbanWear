87c78f27084765059b284dc1585d1358a48d579d
Kevin Bogoreli <bogoreli.kevin@outlook.com>
2025-08-18 18:06:11 -0300

    feat: add sign in with email and password

--- src/app/authentication/components/sign-in-form.tsx
+++ src/app/authentication/components/sign-in-form.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -21,6 +23,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 
 const formSchema = z.object({
   email: z.email("Email inválido"),
@@ -30,6 +33,7 @@ const formSchema = z.object({
 type FormValues = z.infer<typeof formSchema>;
 
 const SignInForm = () => {
+  const router = useRouter();
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -37,9 +41,36 @@ const SignInForm = () => {
       password: "",
     },
   });
-  function onSubmit(values: FormValues) {
-    console.log("Formulario validado:");
-    console.log(values);
+
+  async function onSubmit(values: FormValues) {
+    await authClient.signIn.email({
+      email: values.email,
+      password: values.password,
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: (error) => {
+          if (error.error.code === "USER_NOT_FOUND") {
+            toast.error("Usuário não encontrado");
+            form.setError("email", {
+              message: "Usuário não encontrado",
+            });
+            form.setError("password", {
+              message: "Usuário não encontrado",
+            });
+          } else if (error.error.code === "INVALID_EMAIL_OR_PASSWORD") {
+            toast.error("Email ou senha inválidos");
+            form.setError("email", {
+              message: "Email ou senha inválidos",
+            });
+            form.setError("password", {
+              message: "Email ou senha inválidos",
+            });
+          }
+        },
+      },
+    });
   }
 
   return (
